7e3779ff0388efba5ff81d9321260d08be491a86
king <kingzheng@can-dao.com>
2020-08-11 17:36:40 +0800

    修改启动文件

--- sale-service/src/main/java/com/sunchs/store/sale/SaleApplication.java
+++ sale-service/src/main/java/com/sunchs/store/sale/SaleApplication.java
@@ -3,9 +3,9 @@ package com.sunchs.store.sale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.sunchs.store.db","com.sunchs.store.receive"})
-public class ReceiveApplication {
+@SpringBootApplication(scanBasePackages = {"com.sunchs.store.db","com.sunchs.store.sale"})
+public class SaleApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ReceiveApplication.class, args);
+        SpringApplication.run(SaleApplication.class, args);
     }
 }
